a037f3b3c876db48e8be2ef7c0c40107ccc76d88
João Victor da Silva <jv782063@gmail.com>
2021-06-11 11:55:22 -0300

    creates the page listing all registered people

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,95 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
 
+const pessoasFake = [
+  {
+    id: 1,
+    nome: 'Nome Qualquer 1 Nome Qualquer 1 Nome Qualquer 1',
+    curso: 'Nome de um curso Nome de um curso',
+  },
+  {
+    id: 2,
+    nome: 'Nome Qualquer 2',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 3,
+    nome: 'Nome Qualquer 3',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 4,
+    nome: 'Nome Qualquer 4',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 5,
+    nome: 'Nome Qualquer 5',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 6,
+    nome: 'Nome Qualquer 6',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 7,
+    nome: 'Nome Qualquer 7',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 8,
+    nome: 'Nome Qualquer 8',
+    curso: 'Nome de um curso',
+  },
+  {
+    id: 9,
+    nome: 'Nome Qualquer 9',
+    curso: 'Nome de um curso',
+  },
+];
+
 const Home: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: '#f9f9f9' }}>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>Pessoas cadastradas</Text>
+      </View>
+
+      <View style={styles.pessoasContainer}>
+        <FlatList
+          data={pessoasFake}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <View style={styles.pessoaCard}>
+              <View style={{ flex: 1, paddingRight: 4 }}>
+                <Text style={styles.nome} numberOfLines={1}>
+                  {item.nome}
+                </Text>
+                <Text style={styles.curso} numberOfLines={1}>
+                  {item.curso}
+                </Text>
+              </View>
+              <View style={styles.actions}>
+                <RectButton style={styles.buttonAction}>
+                  <Feather name="edit" size={20} color="#404040" />
+                </RectButton>
+
+                <RectButton style={styles.buttonAction}>
+                  <Feather name="delete" size={20} color="#793636" />
+                </RectButton>
+              </View>
+            </View>
+          )}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,64 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    marginTop: Constants.statusBarHeight
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    marginVertical: 16,
+    color: '#e9e9e9'
+  },
+  pessoasContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  pessoaCard: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#1a1a1a',
+    borderRadius: 8,
+    marginVertical: 8,
+    padding: 12,
+    flexDirection: 'row',
+  },
+  nome: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    color: '#8f8f8f',
+  },
+  curso: {
+    marginTop:4,
+    fontSize: 14,
+    lineHeight: 20,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    color: '#4d4d4d'
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#1f1f1f',
+    borderRadius: 100
+  },
+  buttonAction: {
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default styles;
