5e92eea83ced4e3e989ebcde14b0bfb11bce39de
João Victor da Silva <jv782063@gmail.com>
2021-06-11 12:42:30 -0300

    creates the bar to search for user and also button to create new user

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Alert, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
@@ -55,8 +56,51 @@ const pessoasFake = [
 ];
 
 const Home: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleDelete = () => {
+    Alert.alert(
+      'Confirmação',
+      'Deseja mesmo deletar este registro?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+          onPress: () => console.log('cancelado'),
+        },
+        {
+          text: 'Sim',
+          onPress: () => console.log('sim'),
+        },
+      ],
+      { cancelable: false },
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.searchContainer}>
+          <Feather
+            name="search"
+            size={24}
+            color="#404040"
+            style={{ padding: 12, marginLeft: 4 }}
+          />
+
+          <TextInput
+            style={styles.search}
+            placeholder="Digite o ID do usuário"
+            placeholderTextColor="#333333"
+            keyboardType="numeric"
+          />
+        </View>
+
+        <RectButton style={styles.buttonAction}>
+          <Feather name="user-plus" size={24} color="#4d4d4d" />
+        </RectButton>
+      </View>
+
       <View>
         <Text style={styles.title}>Pessoas cadastradas</Text>
       </View>
@@ -80,7 +124,10 @@ const Home: React.FC = () => {
                   <Feather name="edit" size={20} color="#404040" />
                 </RectButton>
 
-                <RectButton style={styles.buttonAction}>
+                <RectButton
+                  style={styles.buttonAction}
+                  onPress={() => handleDelete()}
+                >
                   <Feather name="delete" size={20} color="#793636" />
                 </RectButton>
               </View>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -9,12 +9,30 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginTop: Constants.statusBarHeight
   },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#1f1f1f',
+    borderRadius: 100,
+    marginRight: 8,
+    flex: 1,
+  },
+  search: {
+    flex: 1,
+    color: '#8f8f8f'
+  },
   title: {
     fontSize: 24,
     lineHeight: 32,
     fontStyle: 'normal',
     fontWeight: 'bold',
-    marginVertical: 16,
+    marginTop: 40,
+    marginBottom: 16,
     color: '#e9e9e9'
   },
   pessoasContainer: {
@@ -56,6 +74,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 8,
     padding: 12,
     borderRadius: 100,
+    backgroundColor: '#1f1f1f',
     justifyContent: 'center',
     alignItems: 'center',
   },
